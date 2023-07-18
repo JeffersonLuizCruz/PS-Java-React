e9cf8cad41d051786fe84bbe341602a0893e947c
JeffersonLuizCruz <jefferson.luiz.cruz@gmail.com>
2023-07-17 22:23:48 -0300

    hotfix: Correção na referencia do nome da tabela do banco de dados

--- src/main/java/br/com/banco/domain/model/BankAccount.java
+++ src/main/java/br/com/banco/domain/model/BankAccount.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
@@ -16,7 +17,7 @@ import lombok.Setter;
 
 @Getter @Setter @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @NoArgsConstructor @AllArgsConstructor
-@Entity
+@Entity @Table(name = "conta")
 public class BankAccount implements Serializable{
 	private static final long serialVersionUID = 1606271053639833932L;
 
